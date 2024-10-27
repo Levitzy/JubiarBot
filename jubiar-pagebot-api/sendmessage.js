d2e13db2b677b62aec7925305cf255f63416645b
Levitzy <levitzy123@gmail.com>
2024-10-28 05:31:16 +0800

    typingIndicator

--- jubiar-pagebot-api/sendmessage.js
+++ jubiar-pagebot-api/sendmessage.js
@@ -6,10 +6,25 @@ const FormData = require('form-data');
 // Read PAGE_ACCESS_TOKEN from token.txt
 const PAGE_ACCESS_TOKEN = fs.readFileSync(path.join(__dirname, '../token.txt'), 'utf8').trim();
 
-module.exports.sendMessage = async (recipientId, message) => {
-    const url = `https://graph.facebook.com/v21.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`;
+const url = `https://graph.facebook.com/v21.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`;
 
+// Function to send typing indicator
+async function sendTypingIndicator(recipientId, action) {
     try {
+        await axios.post(url, {
+            recipient: { id: recipientId },
+            sender_action: action
+        });
+    } catch (error) {
+        console.error('Error sending typing indicator:', error.response ? error.response.data : error.message);
+    }
+}
+
+module.exports.sendMessage = async (recipientId, message) => {
+    try {
+        // Turn on typing indicator
+        await sendTypingIndicator(recipientId, 'typing_on');
+
         if (message.filedata) {
             // If there's a file, use FormData to send it as a file attachment
             const formData = new FormData();
@@ -22,7 +37,7 @@ module.exports.sendMessage = async (recipientId, message) => {
                 headers: formData.getHeaders()
             });
         } else {
-            // Otherwise, send text or other attachments without filedata
+            // Otherwise, send text or other types of attachments
             const data = {
                 recipient: { id: recipientId },
                 message: message.attachment ? { attachment: message.attachment } : { text: message.text }
@@ -33,5 +48,8 @@ module.exports.sendMessage = async (recipientId, message) => {
         console.log('Message sent successfully.');
     } catch (error) {
         console.error('Error sending message:', error.response ? error.response.data : error.message);
+    } finally {
+        // Turn off typing indicator
+        await sendTypingIndicator(recipientId, 'typing_off');
     }
 };
