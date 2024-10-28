88500e07ff17d90b8dfb4728a07acc172cb8e1b5
Levitzy <levitzy123@gmail.com>
2024-10-29 04:42:40 +0800

    fix

--- jubiar-pagebot-api/bot.js
+++ jubiar-pagebot-api/bot.js
@@ -1,15 +1,1 @@
-const { sendTypingIndicator } = require('./sendmessage');
-
-async function setOnlineIndicator(recipientId) {
-    // Turn on the typing indicator
-    await sendTypingIndicator(recipientId, 'typing_on');
-    
-    // Optionally, keep it on periodically or turn it off after a delay
-    setTimeout(async () => {
-        await sendTypingIndicator(recipientId, 'typing_off');
-    }, 5000); // Adjust timing as needed
-}
-
-module.exports = {
-    setOnlineIndicator,
-};
+module.exports = {};
