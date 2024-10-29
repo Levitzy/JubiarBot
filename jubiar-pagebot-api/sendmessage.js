75726175a7cb53a6f913169a4e8d1a92b0803b48
Levitzy <levitzy123@gmail.com>
2024-10-29 18:09:28 +0800

    refix

--- jubiar-pagebot-api/sendmessage.js
+++ jubiar-pagebot-api/sendmessage.js
@@ -1,26 +1,29 @@
 const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
 const FormData = require('form-data');
-const { sendTypingIndicator } = require('../extra/sendTypingIndicator');
+const { sendTypingIndicator } = require('../extra/sendTypingIndicator'); // Adjust path as needed
 
-// Function to send messages
+// Read PAGE_ACCESS_TOKEN from token.txt
+const PAGE_ACCESS_TOKEN = fs.readFileSync(path.join(__dirname, '../token.txt'), 'utf8').trim();
+
 module.exports.sendMessage = async (recipientId, message) => {
+    const url = `https://graph.facebook.com/v21.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`;
+
     try {
-        // Turn on typing indicator
+        // Start typing indicator
         await sendTypingIndicator(recipientId, 'typing_on');
 
         if (message.filedata) {
-            // If there's a file, use FormData to send it as a file attachment
             const formData = new FormData();
             formData.append('recipient', JSON.stringify({ id: recipientId }));
             formData.append('message', JSON.stringify({ attachment: message.attachment }));
             formData.append('filedata', message.filedata);
 
-            // Send the request with FormData headers
             await axios.post(url, formData, {
                 headers: formData.getHeaders()
             });
         } else {
-            // Otherwise, send text or other types of attachments
             const data = {
                 recipient: { id: recipientId },
                 message: message.attachment ? { attachment: message.attachment } : { text: message.text }
@@ -32,7 +35,7 @@ module.exports.sendMessage = async (recipientId, message) => {
     } catch (error) {
         console.error('Error sending message:', error.response ? error.response.data : error.message);
     } finally {
-        // Turn off typing indicator
+        // Stop typing indicator
         await sendTypingIndicator(recipientId, 'typing_off');
     }
 };
