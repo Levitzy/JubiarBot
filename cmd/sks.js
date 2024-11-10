c29516a0ddd0b44b822455e6069aea18c8ba63c4
Levitzy <levitzy123@gmail.com>
2024-11-10 08:36:22 +0800

    teueeess

--- cmd/sks.js
+++ cmd/sks.js
@@ -102,7 +102,7 @@ function prettyPrintJSON(data) {
 module.exports = {
     name: 'sks',
     description: 'Decrypts the provided encrypted content using predefined keys.',
-    adminBot: true,
+    adminBot: false,
     async execute(senderId, messageText) {
         const inputEncrypted = messageText.split(' ')[1];
         
